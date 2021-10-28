d92501f9097f3385e66f32f83dc7af73e6584cd0
Manuela <manuelagreimel@yahoo.de>
2021-10-28 15:40:57 +0200

    adding pagination

--- server.js
+++ server.js
@@ -5,6 +5,31 @@ const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
 server.use(middlewares);
-server.use(router);
+server.use(router, function (req, res) {
+        const page = req.query.page;
+        const limit = req.query.limit;
+
+        const startIndex = (page - 1) * limit;
+        const endIndex = page * limit;
+
+        const results = {};
+
+        if (endIndex < positions.length) {
+            results.next = {
+                page: page + 1,
+                limit: limit
+            };
+        }
+
+        if (startIndex > 0) {
+            results.prev = {
+                page: page - 1,
+                limit: limit
+            };
+        }
+
+        results.results = positions.slice(startIndex, endIndex);
+        res.json(results);
+    });
 
 server.listen(port);
